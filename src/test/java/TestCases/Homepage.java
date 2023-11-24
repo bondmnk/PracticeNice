dec20f75dc3a8fb24db6eb51cf9da841209abc08
Asus <Asus@Rohan>
2023-11-24 12:33:02 +0530

    "adding test case for homepage"

--- src/test/java/TestCases/Homepage.java
+++ src/test/java/TestCases/Homepage.java
@@ -1,5 +1,13 @@
 package TestCases;
 
+import org.testng.annotations.Test;
+
 public class Homepage {
+	@Test
+	public void Tc_home_001(){
+
+		System.out.println("the first test case");
+		
+	}
 
 }
